1264c410f3965b59ee4b84a717c1546403100165
Alejandro Partida <alex.9939@hotmail.com>
2019-11-15 10:15:33 -0600

    added functionality to denuncia and user profiles

---
+++ assets/js/denuncia_profile.js
@@ -0,0 +1,96 @@
+var downloadUrl;
+const tipoReporte = document.getElementById('tipo');
+const lugar = document.getElementById('lugar');
+const descripcion = document.getElementById('descripcion');
+const hacerDenuncia = document.getElementById('denuncia');
+
+
+var userUid;
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        // User is signed in.
+        var userId = user.uid;
+        userUid = userId;
+    } else {
+        // No user is signed in.
+        
+    }
+});
+
+
+async function fotoDenuncia() {
+
+    const { value: file } = await Swal.fire({
+        title: 'Seleccione una imagen',
+        input: 'file',
+        inputAttributes: {
+            'accept': 'image/*',
+            'aria-label': 'Archivo seleccionado.'
+        }
+    });
+
+    if (file) {
+        const reader = new FileReader;
+        reader.onload = (e) => {
+            Swal.fire({
+                title: 'Tu foto ha sido subida',
+                imageUrl: e.target.result,
+                imageAlt: 'Tu foto ha sido subida'
+            });
+            var storageRef = firebase.storage().ref();
+            var coverPicsRef = storageRef.child('fotosDenuncias');
+            var timestamp = Number(new Date());
+            var fileType = file.type.split('/').pop();
+            console.log(fileType);
+            var fileChild = coverPicsRef.child(timestamp.toString() + '.' + fileType);
+            fileChild.put(file).then(function (snapshot) {
+                console.log('¡Se cargó una foto de denuncia!');
+                document.getElementById('foto').disabled = true;
+                fileChild.getDownloadURL().then(function (url) {
+                     downloadUrl = url.toString();
+                    
+                })
+            });
+
+
+        };
+        reader.readAsDataURL(file);
+    }
+    else {
+        console.log("error");
+    }
+
+}
+
+function clearModalInputs() {
+    document.getElementById('tipo').value = "";
+    document.getElementById('lugar').value = "";
+    document.getElementById('descripcion').value = "";
+    document.getElementById('foto').disabled = false;
+    
+}
+
+function addDenuncia() {
+    console.log(descripcion.value                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+        );
+    
+    db.collection('denuncias').add({
+        sentBy: userUid,
+        tipoReporte: tipoReporte.value,
+        lugar: lugar.value,
+        descripcion: descripcion.value,
+        estatus: 'Pendiente',
+        numComprobaciones: 0,
+        pictureURL: downloadUrl
+
+    }).then(a => {
+        Swal.fire({
+            type: 'success',
+            title: 'Denuncia publicada.',
+            text: '¡Gracias por tu denuncia, será atendida en breve!'
+        });
+    });
+
+    hacerDenuncia.setAttribute('data-dismiss', 'modal');
+   
+}
--- assets/js/user_profile.js
+++ assets/js/user_profile.js
@@ -1,8 +1,57 @@
+var userUid;
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        
+        // User is signed in.
+        var userId = user.uid;
+        userUid = userId;
+        db.collection('users').doc(userId).get().then((doc => {
+            var username = document.getElementById('username');
+            var poc = document.getElementById('poc');
+            var number = document.getElementById('number');
+            var gender = document.getElementById('gender');
+            var address = document.getElementById('address');
+
+            console.log(doc.data().username);
+            
+
+            username.value = doc.data().username;
+            number.value = doc.data().phonenumber;
+            gender.value = doc.data().gender;
+            address.value = doc.data().address;
+            poc.innerHTML = `<h3><span class="counter">${doc.data().poc}</span></h3>`;
+        }));
+    } else {
+        // No user is signed in.
+        
+    }
+});
+
 function edit() {
     document.getElementById('username').disabled = false;
-    document.getElementById('email').disabled = false;
     document.getElementById('number').disabled = false;
     document.getElementById('gender').disabled = false;
     document.getElementById('address').disabled = false;
     document.getElementById("botones").style.display = "block";
+}
+
+function cancelar() {
+    var username = document.getElementById('username');
+    var number = document.getElementById('number');
+    var gender = document.getElementById('gender');
+    var address = document.getElementById('address');
+    document.getElementById("botones").style.display = "none";
+
+    username.disabled = true;
+    number.disabled = true;
+    gender.disabled = true;
+    address.disabled = true;
+
+    db.collection('users').doc(userUid).get().then(doc => {
+        username.value = doc.data().username;
+        number.value = doc.data().phonenumber;
+        gender.value = doc.data().gender;
+        address.value = doc.data().address;
+
+    })
 }
