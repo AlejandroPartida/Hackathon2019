26e3a23cf156fbd50a66f7c39a083f1fcb5ba116
Alejandro Partida <alex.9939@hotmail.com>
2019-11-14 21:16:41 -0600

    Added inicialization file.

---
+++ assets/js/inicio.js
@@ -0,0 +1,16 @@
+src="https://www.gstatic.com/firebasejs/7.4.0/firebase-app.js";
+
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyDTWG8rVRfXL3FK7bjzuLWTwFTpjhyYVR8",
+    authDomain: "hackathon2019-b9401.firebaseapp.com",
+    databaseURL: "https://hackathon2019-b9401.firebaseio.com",
+    projectId: "hackathon2019-b9401",
+    storageBucket: "hackathon2019-b9401.appspot.com",
+    messagingSenderId: "101563286553",
+    appId: "1:101563286553:web:8fa706f353dfb1206c5688",
+    measurementId: "G-4Z0GJ4Q6NK"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
